d682bab02c3146fba14a2139ebef54e7d60903ad
Sreekanth <sreekfreak995@gmail.com>
2020-08-21 19:45:07 +0530

    It's show time yo

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/android_kernel_xiaomi_phoenix.git -b ten kernel
+git clone --depth=1 https://github.com/stormbreaker-project/phoenix.git -b ten kernel
 cd kernel
 git apply ./90hz.patch
 bash build.sh
