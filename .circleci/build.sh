4ad5017efdf8ed3c2364194b76de500c47d51838
Palakurthi Sreekanth <sreekfreak995@gmail.com>
2020-10-29 23:48:50 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/Area42-Labs/kernel_xiaomi_phoenix.git -b ten kernel
+git clone --depth=1 https://github.com/Area42-Labs/kernel_xiaomi_phoenix.git -b eleven  kernel
 cd kernel
 bash build.sh
