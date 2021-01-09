57acaa0f7ff533cd4fc73c6e5461f35beef3da9e
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-01-09 20:56:50 +0530

    Ayyyo

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_phoenix.git -b eleven  kernel
+git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_phoenix.git -b arrow-11.0  kernel
 cd kernel
 bash build.sh
